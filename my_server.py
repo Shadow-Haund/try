87dde9c7c239e1a31cdebfdf77e1ac61b0e2b28a
Edward <edvard.dunijsk@mail.ru>
2021-11-25 23:38:54 +0300

    make server async

--- my_server.py
+++ my_server.py
@@ -1,3 +1,4 @@
+import asyncio
 import socket
 import threading
 from datetime import datetime
@@ -11,51 +12,50 @@ port = 55555
 file_end = '37e3f4a8-b8c9-4f22-ad4d-8bd81e686822'
 length_of_message = len(f"file{file_end}")
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((hostS, port))
-server.listen()
-u_sockets = []
+writers = []
 
 
 def broadcast(message, user):
-    for client in u_sockets:
+    for client in writers:
         if client != user:
-            client.send(message)
+            client.write(message)
 
 
-def handle(client):
+async def handle(reader, writer):
+    writers.append(writer)
     buffer = 0
     full_mes = bytes()
     full_file = bytes()
     while True:
-        # try:
         if buffer == 0:
-            message = client.recv(length_of_message).decode(code_table)
+            message = await reader.read(length_of_message)
+            message = message.decode(code_table)
             if message.strip().isdigit():  # если число, то это просто сообщение и нужно изменить длину буфера
                 buffer = int(message.strip())
             if message == f"file{file_end}":  # если file, то хотят отправить файл
-                file_header_len = int(client.recv(length_of_message).decode())
-                file_name_size = client.recv(file_header_len).decode(
-                    code_table)  # заголовок это имя файла и размер файла
+                file_header_len = await reader.read(length_of_message)
+                file_header_len = int(file_header_len.decode(code_table))
+                file_name_size = await reader.read(file_header_len)  # заголовок это имя файла и размер файла
+                file_name_size = file_name_size.decode(code_table)
                 file_size = file_name_size[file_name_size.find("<>") + 2:]
                 file_size = int(file_size)
-                file_data_read = client.recv(file_size)
+                file_data_read = await reader.read(file_size)
                 full_file += file_data_read
                 while not file_end.encode(code_table) in full_file:
-                    file_data_read = client.recv(file_size)
+                    file_data_read = await reader.read(file_size)
                     full_file += file_data_read
                 else:
                     broadcast(f'{f"file{file_end}":<{len(f"file{file_end}".encode(code_table))}}'.encode(code_table) +
                               f'{len(file_name_size.encode(code_table)):<{length_of_message}}'.encode(
                                   code_table) +
-                              file_name_size.encode(code_table) + full_file, client)
+                              file_name_size.encode(code_table) + full_file, writer)
                     print("serF2")
         else:
-            message = client.recv(buffer)
+            message = await reader.read(buffer)
             full_mes += message
             # это часть модернизируется с целью успешного получения всего сообщения при выкладке сервера удаленно
             while not file_end.encode(code_table) in full_mes:
-                message = client.recv(buffer)
+                message = await reader.read(buffer)
                 full_mes += message
             else:
                 time_zone = full_mes[
@@ -66,18 +66,19 @@ def handle(client):
                                + full_mes[full_mes.find('>'.encode(code_table)) + 1:]
                 message_len_send = f'{len(message_send):<{length_of_message}}'.encode(
                     code_table)
-                broadcast(message_len_send + message_send, client)
+                broadcast(message_len_send + message_send, writer)
                 print("sev2")
                 buffer = 0
 
 
-def receive_connection():
+async def receive_connection():
     while True:
-        client, address = server.accept()
-        print(f"Connected with {str(address)}")
-        u_sockets.append(client)
-        thread = threading.Thread(target=handle, args=(client,))
-        thread.start()
+        server = await asyncio.start_server(
+            handle, hostL, port)
+        addr = server.sockets[0].getsockname()
+        print(f'Serving on {addr}')
+        async with server:
+            await server.serve_forever()
 
 
-receive_connection()
+asyncio.run(receive_connection())
